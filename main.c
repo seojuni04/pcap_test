c68a7b6ab2dafbd62694a3aef0934fcb41b3c300
seojuni04 <seojuni.kim@gmail.com>
2017-07-17 08:46:01 -0700

    add print Ethernet Header

--- main.c
+++ main.c
@@ -3,18 +3,21 @@
 
 int main(int argc, char *argv[])
 {
-    pcap_t *handle;			/* Session handle */
-    char *dev;			/* The device to sniff on */
-    char errbuf[PCAP_ERRBUF_SIZE];	/* Error string */
-    struct bpf_program fp;		/* The compiled filter */
-    char filter_exp[] = "port 23";	/* The filter expression */
-    bpf_u_int32 mask;		/* Our netmask */
-    bpf_u_int32 net;		/* Our IP */
-    struct pcap_pkthdr header;	/* The header that pcap gives us */
-    const u_char *packet;		/* The actual packet */
+    pcap_t *handle;         /* Session handle */
+    char *dev;         /* The device to sniff on */
+    char errbuf[PCAP_ERRBUF_SIZE];   /* Error string */
+    struct bpf_program fp;      /* The compiled filter */
+    char filter_exp[] = "port 80";   /* The filter expression */
+    bpf_u_int32 mask;      /* Our netmask */
+    bpf_u_int32 net;      /* Our IP */
+    struct pcap_pkthdr header;   /* The header that pcap gives us */
+    const u_char *packet;      /* The actual packet */
+    int pcap_mod;
+    int sig = 0;
 
     /* Define the device */
     dev = pcap_lookupdev(errbuf);
+
     if (dev == NULL) {
         fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
         return(2);
@@ -29,6 +32,7 @@ int main(int argc, char *argv[])
 
     /* Open the session in promiscuous mode */
     handle = pcap_open_live(dev, BUFSIZ, 1, 1000, errbuf);
+
     if (handle == NULL) {
         fprintf(stderr, "Couldn't open device %s: %s\n", dev, errbuf);
         return(2);
@@ -39,21 +43,33 @@ int main(int argc, char *argv[])
         fprintf(stderr, "Couldn't parse filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return(2);
     }
-
     if (pcap_setfilter(handle, &fp) == -1) {
         fprintf(stderr, "Couldn't install filter %s: %s\n", filter_exp, pcap_geterr(handle));
         return(2);
     }
 
-    while(1){
-        /* Grab a packet */
-        packet = pcap_next(handle, &header);
+    /* Grab a packet */
+    while((pcap_mod=pcap_next_ex(handle, &header, &packet))>=0)
+    {
+        if(0 == sig)
+            printf("No packets found.\n");
 
-        /* Print its length */
-        printf("Jacked a packet with length of [%d]\n", header.len);
+        if(pcap_mod == 0)
+        {
+            sig = 0;
+            continue;
+        }
+        sig = 1;
+        /* Print Ethernet Header */
+        printf("=================================================\n");
+        printf("================ Ethernet Header ================\n");
+        printf("=================================================\n");
+        printf("Destination Mac Address: %02x:%02x:%02x:%02x:%02x:%02x \n",packet[0],packet[1],packet[2],packet[3],packet[4],packet[5]);
+        printf("Source Mac Address: %02x:%02x:%02x:%02x:%02x:%02x \n",packet[6],packet[7],packet[8],packet[9],packet[10],packet[11]);
     }
-    /* And close the session */
+
+    /* close the session */
     pcap_close(handle);
 
     return(0);
-    }
+}
